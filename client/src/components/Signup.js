7751b51092004496e4b707eeb32d82a94e535803
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2021-01-30 14:44:15 -0700

    working on connection and forms

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export class Signup extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+      buttonClicked: false
+    };
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  sendForm () {
+    if (!this.state.buttonClicked) {
+      this.setState({
+        buttonClicked: true
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <h3>Signup for news.</h3>
-      </div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+          <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" />
+        </Form.Group>
+        <Button variant="primary" onClick = {this.sendForm}>
+          Submit
+        </Button>
+      </Form>
+    </div>
     )
   }
-}
+};
 
 export default Signup
