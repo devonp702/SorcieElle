ec349438009f5f3aa181ae74e7d38cbb37443eb4
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2021-02-05 15:44:36 -0700

    reworking the form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -5,43 +5,31 @@ import axios from "axios";
 
 export class Signup extends Component {
   constructor(){
-    super();
+    super(props);
     this.state = {
-      data: [],
+      email: String,
+      name: String,
       buttonClicked: false
     };
-    this.sendForm = this.sendForm.bind(this);
   }
 
-  sendForm () {
-    if (!this.state.buttonClicked) {
-      this.setState({
-        buttonClicked: true
-      });
-      alert("sign-up complete!");
-      
-    }
-  }
+  postDataHandler = () => {
+    const data = {
+        name: this.state.name,
+        email: this.state.email
+    };
+    axios.post('https://localhost:3001/', data)
+        .then(response => {
+            console.log(response);
+        });
+}
 
   render() {
     return (
       <div>
         <h3>Get updates here.</h3>
         <Form>
-          <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-          <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Name" />
-        </Form.Group>
-        <Button variant="primary" onClick = {this.sendForm}>
-          Submit
-        </Button>
+          
       </Form>
     </div>
     )
